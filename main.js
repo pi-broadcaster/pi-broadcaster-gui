78469ef063dae45283f9d36e39ca2b26aa1cc04d
Phạm Tuyên <phamthanhtuyen2k8@gmail.com>
2023-07-08 15:04:02 +0700

    ok second

--- main.js
+++ main.js
@@ -1,13 +1,32 @@
-const {app, BrowserWindow, ipcMain} = require("electron")
+const {app, BrowserWindow, ipcMain, nativeTheme, globalShortcut} = require("electron")
 const path = require("path")
 
 function createWindow() {
     const win = new BrowserWindow({
+        autoHideMenuBar: true,
         webPreferences: {
             preload: path.join(__dirname, "src/script/preload.js")
         }
     })
 
+    win.setIcon(path.join(__dirname, "src/icon.ico"))
     win.loadFile(path.join(__dirname, "src/index.html"))
     win.maximize()
 }
+
+function toggleTheme() {
+   if (nativeTheme.shouldUseDarkColors) nativeTheme.themeSource = "light"
+   else nativeTheme.themeSource = "dark"
+}
+
+app.whenReady().then(() => {
+    globalShortcut.register("CommandorControl+Alt+T", () => {
+        toggleTheme()
+    })
+    createWindow()
+    if (BrowserWindow.getAllWindows().length === 0) createWindow()
+})
+
+app.on("window-all-closed", () => {
+    if (process.platform !== "darwin") app.quit()
+})
