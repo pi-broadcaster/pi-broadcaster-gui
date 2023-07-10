ad330b06c6288cc40c6dcf33499e631b86986bb9
Phạm Tuyên <phamthanhtuyen2k8@gmail.com>
2023-07-10 10:55:22 +0700

    complete gui

--- src/script/renderrer.js
+++ src/script/renderrer.js
@@ -31,7 +31,7 @@ var color = await window.theme.get()
 theme.innerText = color + "_mode"
 theme.title = "Chuyển sang nền "
 theme.title += (color == "light") ? "sáng" : "tối"
-table.className = "table table-hover table-bordered table-striped table-"
+table.className = "table table-hover table-bordered table-"
 table.className += (color == "light") ? "dark" : "light"
 triggerTooltip()
 theme.addEventListener("click", async () => {
@@ -40,24 +40,41 @@ theme.addEventListener("click", async () => {
     theme.innerText = color + "_mode"
     theme.title = "Chuyển sang nền "
     theme.title += (color == "light") ? "sáng" : "tối"
-    table.className = "table table-hover table-bordered table-striped table-"
+    table.className = "table table-hover table-bordered table-"
     table.className += (color == "light") ? "dark" : "light"
     triggerTooltip()
 })
 
-var config = await window.config.read()
+var conf = await window.config.read()
 var content = []
 const cnt = document.getElementById("content")
-for (var i = 0; i < config.length; i++) {
+for (var i = 0; i < conf.length; i++) {
     content.push(cnt.insertRow())
     for (var index = 0; index < 4; index++) {
         content[i].insertCell()
     }
 }
-for (var i = 0; i < config.length; i++) {
+
+function minTommss(minutes){
+    var sign = minutes < 0 ? "-" : "";
+    var min = Math.floor(Math.abs(minutes))
+    var sec = Math.floor((Math.abs(minutes) * 60) % 60);
+    return sign + (min < 10 ? "0" : "") + min + ":" + (sec < 10 ? "0" : "") + sec;
+}
+
+for (var i = 0; i < conf.length; i++) {
     content[i].cells[0].innerHTML = (i + 1).toString() 
-    content[i].cells[1].innerHTML = (config[i].type == "text") ? "" : "https://www.youtube.com/playlist?list="
-    content[i].cells[1].innerHTML += config[i].id
+    var prestr = (conf[i].type == 'text') ? '' : 'https://www.youtube.com/playlist?list='
+    var mainstr = conf[i].id
+    content[i].cells[1].innerHTML = `<div>${prestr}${mainstr}</div>`
+    /*content[i].addEventListener("mouseover", function() {
+        this.getElementByTagName("div")[0].style.backgroundColor = "#323539"
+    })*/
+    content[i].cells[2].innerHTML = minTommss(conf[i].time)
+    content[i].cells[3].innerHTML = ""
+    conf[i].day.sort()
+    conf[i].day.forEach(wkday => content[i].cells[3].innerHTML += (wkday + 2).toString() + ", ")
+    content[i].cells[3].innerHTML = content[i].cells[3].innerHTML.substr(0, content[i].cells[3].innerHTML.length - 2)
 }
 
 
